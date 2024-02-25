3513d873902e1ab0f6fb0042b3aa50dae7b91bc3
Restu Ramadhan <67189204+Randevsss3@users.noreply.github.com>
2024-02-25 11:11:10 +0000

    Variabel

---
+++ variable.dart
@@ -0,0 +1,11 @@
+variabel(){
+
+  // variabel adalah objek yang menyimpan data
+  
+  String firstName = 'Restu';
+  String lastName = 'Ramadhan';
+
+  var fullName = firstName + " " + lastName;
+
+  print(fullName);
+}
