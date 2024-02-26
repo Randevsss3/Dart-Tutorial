161e081b09ef8113b362b59a61e91d0d4b3706f3
Restu Ramadhan <67189204+Randevsss3@users.noreply.github.com>
2024-02-26 04:29:14 +0000

    Tipe data bool

--- tipedata.dart
+++ tipedata.dart
@@ -11,14 +11,27 @@ tipedata(){
   num= tipedata yang bernilai bisa bilangan bulat maupun desimal
   */
 
-  //int
+  //integer
   int bilBulat = 123;
   print(bilBulat);
-
+  
+  //double
   double bilDesimal = 10.5;
   print(bilDesimal);
 
+  //num
   num bilDinamis = 123.10;
   print(bilDinamis);
+
+  bilDinamis = 321;
+  print(bilDinamis);
+  
+
+  // boolean adalah tipe data yang bernilai benar atau salah / true atau false
+  bool hariSenin = false;
+  print(hariSenin);
+
+  hariSenin = true;
+  print(hariSenin);
     
 }
