eb1af0b868592ba73394ee21734860fbbefcec3c
Restu Ramadhan <67189204+Randevsss3@users.noreply.github.com>
2024-02-28 04:46:13 +0000

    Hasil ganti nama dan perubahan folder

---
+++ konversi tipe data/numtostring.dart
@@ -0,0 +1,36 @@
+//konversi tipe data
+
+konversiTpDt(){
+
+  //String to Number
+
+  //tipedata(num).parse(variable string)
+
+  var angka = '1000';
+  var angkabulat = int.parse(angka);
+  var angkadouble = double.parse(angka);
+
+  print(angkabulat);
+  print(angkadouble);
+
+  print(''' ''');
+
+  //integer ke double
+  var intToDouble = angkabulat.toDouble();
+  print(intToDouble);
+
+  //double ke integer
+  var doubleToInt = angkadouble.toInt();
+  print(doubleToInt);
+  
+  print("");
+
+  //String dari int
+  var intToString = doubleToInt.toString();
+  print('int ke string  ' + intToString);
+
+  var doubleToString = intToDouble.toString();
+  print('double ke string' + ' ' +  doubleToString);
+
+  
+}
